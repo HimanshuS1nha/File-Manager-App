85c51290e14d80ae9857a3b492a997a5d05b7127
Himanshu Sinha <ashusinha1904@gmail.com>
2025-03-26 19:34:31 +0530

    Permission requesting functionality added

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,147 @@
-import { router, useRootNavigationState } from "expo-router";
-import { useEffect } from "react";
-import { Text, View, ActivityIndicator, Image } from "react-native";
+import {
+  Text,
+  View,
+  ActivityIndicator,
+  Image,
+  Platform,
+  Alert,
+  BackHandler,
+} from "react-native";
+import { useCallback, useEffect } from "react";
 import tw from "twrnc";
+import { router, useRootNavigationState } from "expo-router";
+import {
+  PERMISSIONS,
+  request,
+  checkMultiple,
+  RESULTS,
+} from "react-native-permissions";
+import {
+  checkManagePermission,
+  requestManagePermission,
+} from "manage-external-storage";
+import * as Linking from "expo-linking";
 
 export default function Index() {
   const rootNavigationState = useRootNavigationState();
+
+  const requestPermission = useCallback(async () => {
+    if (Platform.OS === "android") {
+      if (Platform.Version >= 30) {
+        const permissionStatus = await checkMultiple([
+          PERMISSIONS.ANDROID.READ_MEDIA_AUDIO,
+          PERMISSIONS.ANDROID.READ_MEDIA_IMAGES,
+          PERMISSIONS.ANDROID.READ_MEDIA_VIDEO,
+        ]);
+
+        if (
+          permissionStatus["android.permission.READ_MEDIA_AUDIO"] !==
+          RESULTS.GRANTED
+        ) {
+          const permission = await request(
+            PERMISSIONS.ANDROID.READ_MEDIA_AUDIO
+          );
+          if (permission !== RESULTS.GRANTED) {
+            return false;
+          }
+        }
+
+        if (
+          permissionStatus["android.permission.READ_MEDIA_IMAGES"] !==
+          RESULTS.GRANTED
+        ) {
+          const permission = await request(
+            PERMISSIONS.ANDROID.READ_MEDIA_IMAGES
+          );
+          if (permission !== RESULTS.GRANTED) {
+            return false;
+          }
+        }
+
+        if (
+          permissionStatus["android.permission.READ_MEDIA_VIDEO"] !==
+          RESULTS.GRANTED
+        ) {
+          const permission = await request(
+            PERMISSIONS.ANDROID.READ_MEDIA_VIDEO
+          );
+          if (permission !== RESULTS.GRANTED) {
+            return false;
+          }
+        }
+
+        const manageExternalStoragePermission = await checkManagePermission();
+        if (!manageExternalStoragePermission) {
+          const permission = await requestManagePermission();
+          if (!permission) {
+            return false;
+          }
+        }
+      } else {
+        const permissionStatus = await checkMultiple([
+          PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+          PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE,
+        ]);
+
+        if (
+          permissionStatus["android.permission.READ_EXTERNAL_STORAGE"] !==
+          RESULTS.GRANTED
+        ) {
+          const readPermission = await request(
+            PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE
+          );
+          if (readPermission !== RESULTS.GRANTED) {
+            return false;
+          }
+        }
+
+        if (
+          permissionStatus["android.permission.WRITE_EXTERNAL_STORAGE"] !==
+          RESULTS.GRANTED
+        ) {
+          const writePermission = await request(
+            PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE
+          );
+          if (writePermission !== RESULTS.GRANTED) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }, []);
 
   useEffect(() => {
     let timeout: NodeJS.Timeout;
 
     if (rootNavigationState?.key) {
-      timeout = setTimeout(() => {
-        router.replace("/home");
-      }, 500);
+      requestPermission()
+        .then((res) => {
+          if (res) {
+            timeout = setTimeout(() => {
+              router.replace("/home");
+            }, 500);
+          } else {
+            Alert.alert(
+              "Error",
+              "This permission is required for the app to work",
+              [
+                {
+                  text: "Exit",
+                  onPress: BackHandler.exitApp,
+                },
+                {
+                  text: "Ok",
+                  onPress: Linking.openSettings,
+                },
+              ]
+            );
+          }
+        })
+        .catch(() => {
+          Alert.alert("Error", "Error occured while checking permissions");
+        });
     }
 
     return () => {
