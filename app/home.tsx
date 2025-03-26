6427ac1cf023395afeaae6ce9e49312df4011e8e
Himanshu Sinha <ashusinha1904@gmail.com>
2025-03-26 22:11:32 +0530

    home page UI updated

--- app/home.tsx
+++ app/home.tsx
@@ -4,11 +4,38 @@ import tw from "twrnc";
 
 import StorageCard from "@/components/storage-card";
 
+import { options } from "@/constants/options";
+
 const Home = () => {
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <View style={tw`p-5 gap-y-6`}>
         <StorageCard />
+
+        <View style={tw`gap-5`}>
+          {options.map((option, i) => {
+            return (
+              <View key={i} style={tw`flex-row justify-between items-center`}>
+                {option.map((item) => {
+                  return (
+                    <View key={item.title} style={tw`items-center gap-y-2`}>
+                      <View
+                        style={tw`size-16 items-center justify-center bg-white rounded-lg shadow-lg shadow-white`}
+                      >
+                        <item.Icon
+                          name={item.iconName}
+                          size={30}
+                          color="#4F46E5"
+                        />
+                      </View>
+                      <Text style={tw`font-medium`}>{item.title}</Text>
+                    </View>
+                  );
+                })}
+              </View>
+            );
+          })}
+        </View>
       </View>
     </ScrollView>
   );
---
+++ constants/options.ts
@@ -0,0 +1,53 @@
+import {
+  AntDesign,
+  Entypo,
+  FontAwesome,
+  FontAwesome5,
+} from "@expo/vector-icons";
+
+export const options = [
+  [
+    {
+      Icon: FontAwesome5,
+      iconName: "images",
+      title: "Images",
+    },
+    {
+      Icon: Entypo,
+      iconName: "folder-video",
+      title: "Videos",
+    },
+    {
+      Icon: FontAwesome5,
+      iconName: "music",
+      title: "Audio",
+    },
+    {
+      Icon: FontAwesome,
+      iconName: "file",
+      title: "Documents",
+    },
+  ],
+  [
+    {
+      Icon: AntDesign,
+      iconName: "android1",
+      title: "Apk",
+    },
+    {
+      Icon: FontAwesome,
+      iconName: "file-zip-o",
+      title: "Zip",
+    },
+    {
+      Icon: FontAwesome,
+      iconName: "download",
+      title: "Downloads",
+    },
+    {
+      Icon: FontAwesome,
+      iconName: "star",
+      title: "Favourites",
+    },
+  ],
+];
