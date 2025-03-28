e482f32daef23469a541a0e365f56e59838e75f8
Himanshu Sinha <ashusinha1904@gmail.com>
2025-03-28 16:42:22 +0530

    folder-preview updated

--- components/folder-preview.tsx
+++ components/folder-preview.tsx
@@ -1,13 +1,25 @@
-import { View, Text } from "react-native";
+import { Text, Pressable } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import { FontAwesome } from "@expo/vector-icons";
+import { router } from "expo-router";
 
 import type { FileOrFolderType } from "@/types";
 
 const FolderPreview = ({ folder }: { folder: FileOrFolderType }) => {
   return (
-    <View style={tw`flex-row gap-x-5 items-center px-2 py-2.5`}>
+    <Pressable
+      style={tw`flex-row gap-x-5 items-center px-2 py-2.5`}
+      onPress={() =>
+        router.push({
+          pathname: "/folders",
+          params: {
+            type: folder.name,
+            path: folder.path,
+          },
+        })
+      }
+    >
       <FontAwesome name="folder" size={42} color="#4F46E5" />
 
       <Text style={tw`font-medium text-base`}>
@@ -15,7 +27,7 @@ const FolderPreview = ({ folder }: { folder: FileOrFolderType }) => {
           ? folder.name.substring(0, 30) + "..."
           : folder.name}
       </Text>
-    </View>
+    </Pressable>
   );
 };
 
