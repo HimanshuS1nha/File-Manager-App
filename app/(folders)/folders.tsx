a5744cdbcd8fe45525470efcac83d86ecbe26ffa
Himanshu Sinha <ashusinha1904@gmail.com>
2025-03-28 16:40:37 +0530

    folders page added

---
+++ app/(folders)/folders.tsx
@@ -0,0 +1,62 @@
+import { View, Text, ActivityIndicator } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { useQuery } from "@tanstack/react-query";
+import { readDir, ExternalStorageDirectoryPath } from "react-native-fs";
+
+import CustomSectionList from "@/components/custom-section-list";
+
+import { groupAndSortByDate } from "@/utils/group-and-sort-by-date";
+import { getFileType } from "@/utils/get-file-type";
+
+const Folders = () => {
+  const { title, path } = useLocalSearchParams() as {
+    title: string;
+    path?: string;
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: [`get-${title}`],
+    queryFn: async () => {
+      const filesAndFolders = await readDir(
+        path ?? ExternalStorageDirectoryPath + `/${title}`
+      );
+
+      return groupAndSortByDate(
+        [],
+        filesAndFolders.map((item) => ({
+          id: item.path,
+          path: item.path,
+          modificationTime: new Date(item.mtime ?? 0).getTime(),
+          name: item.name,
+          type: item.isDirectory() ? "folder" : "file",
+          uri: `file://${item.path}`,
+          fileType: getFileType(item.name),
+          size: item.size,
+        }))
+      );
+    },
+  });
+  return (
+    <View style={tw`flex-1 bg-white`}>
+      <Stack.Screen options={{ title }} />
+
+      <View style={tw`px-2 pt-1.5`}>
+        {isLoading ? (
+          <ActivityIndicator size={40} color={"blue"} />
+        ) : data && data.length > 0 ? (
+          <CustomSectionList data={data} />
+        ) : (
+          <View style={tw`items-center`}>
+            <Text style={tw`text-rose-600 font-semibold`}>
+              No data to show.
+            </Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Folders;
--- constants/options.ts
+++ constants/options.ts
@@ -11,21 +11,37 @@ export const options = [
       Icon: FontAwesome5,
       iconName: "images",
       title: "Images",
+      url: "/images-videos-audio",
+      params: {
+        type: "Images",
+      },
     },
     {
       Icon: Entypo,
       iconName: "folder-video",
       title: "Videos",
+      url: "/images-videos-audio",
+      params: {
+        type: "Videos",
+      },
     },
     {
       Icon: FontAwesome5,
       iconName: "music",
       title: "Audio",
+      url: "/images-videos-audio",
+      params: {
+        type: "Audio",
+      },
     },
     {
       Icon: FontAwesome,
       iconName: "file",
       title: "Documents",
+      url: "/folders",
+      params: {
+        title: "Documents",
+      },
     },
   ],
   [
@@ -43,6 +59,10 @@ export const options = [
       Icon: FontAwesome,
       iconName: "download",
       title: "Downloads",
+      url: "/folders",
+      params: {
+        title: "Download",
+      },
     },
     {
       Icon: FontAwesome,
