b5d63654946cbb8e968497584f78c528b87f842c
Himanshu Sinha <ashusinha1904@gmail.com>
2025-03-29 00:22:05 +0530

    Add functionality to add and remove favourites

--- app/(file)/_layout.tsx
+++ app/(file)/_layout.tsx
@@ -1,11 +1,22 @@
 import { View, Pressable } from "react-native";
-import React from "react";
+import React, { useMemo } from "react";
 import tw from "twrnc";
 import { Stack } from "expo-router";
 import { AntDesign, FontAwesome6 } from "@expo/vector-icons";
 import { router } from "expo-router";
 
+import { useSelectedFile } from "@/hooks/use-selected-file";
+import { useFavourites } from "@/hooks/use-favourites";
+
 const FileLayout = () => {
+  const selectedFile = useSelectedFile((state) => state.selectedFile);
+  const favourites = useFavourites((state) => state.favourites);
+  const updateFavourites = useFavourites((state) => state.updateFavourites);
+
+  const isFavourite = useMemo(
+    () => !!favourites.find((favourite) => favourite.id === selectedFile?.id),
+    [favourites, selectedFile]
+  );
   return (
     <Stack
       screenOptions={{
@@ -27,7 +38,13 @@ const FileLayout = () => {
           return (
             <View style={tw`flex-row gap-x-5 items-center`}>
               <AntDesign name="sharealt" size={24} color="white" />
-              <AntDesign name={"staro"} size={24} color="white" />
+              <Pressable onPress={() => updateFavourites(selectedFile!)}>
+                <AntDesign
+                  name={isFavourite ? "star" : "staro"}
+                  size={24}
+                  color="white"
+                />
+              </Pressable>
               <FontAwesome6 name="ellipsis-vertical" size={24} color="white" />
             </View>
           );
